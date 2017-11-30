61d4578d7b77887183dea0a6e478b5fcccfec356
Rebecca <rebecca.kane@outlook.com>
2017-11-30 20:04:00 +0000

    Converting image to Base64 & tsending to server

--- home.py
+++ home.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory, json
 import os
 from werkzeug.utils import secure_filename
 
@@ -8,43 +8,13 @@ app = Flask(__name__)
 def index():
     return app.send_static_file('index.html') # Load index.html file
 
-# http://127.0.0.1:5000/digit/9 outputs digit:9
-'''
-@app.route('/digit/<digit>')
-def fname(digit): 
-    return 'Digit:' + digit
-'''
-
-# File upload adapted from http://flask.pocoo.org/docs/0.12/patterns/fileuploads/
-UPLOAD_FOLDER = './uploads/'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-def allowed_file(filename):
-    return '.' in filename and \
-        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-@app.route('/', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            return file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            #return redirect()
-            #return uploaded_file(filename)
-            #return #redirect(url_for('upload_file')) #uploaded_file(filename) #jsonify({"result": randint(0,9)}), 200 #redirect(url_for('uploaded_file' filename=filename))
-    return
+# Save the image
+@app.route('/save', methods=['POST'])
+def save():
+    #file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    img =  request.json
+    #print(img) # Outputs JSON data to console, test if base64 transferred
+    return json.dumps(img)
 
 '''
 @app.route('/uploadedImage')
