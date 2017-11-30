86392dfe501a26f436a527401bab0eceaf9b2b04
Rebecca <rebecca.kane@outlook.com>
2017-11-30 20:38:30 +0000

    Creating file of base64 data

--- home.py
+++ home.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory, json
-import os
+import os, base64
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
@@ -14,6 +14,21 @@ def save():
     #file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
     img =  request.json
     #print(img) # Outputs JSON data to console, test if base64 transferred
+    #imgdata = base64.b64decode(img)
+    #filename = 'some_image.jpg'  # I assume you have a way of picking unique filenames
+    #with open(filename, 'wb') as f:
+    #    f.write(imgdata)
+
+    with open('img', 'w') as outfile:
+        json.dump(img, outfile)
+        
+    newjpgtxt = open(outfile,"rb").read()
+
+    g = open("out.jpg", "w")
+    g.write(newjpgtxt.decode('base64'))
+    g.close()
+
+
     return json.dumps(img)
 
 '''
